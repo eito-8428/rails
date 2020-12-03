bc0f526e84ee8c82054d9e628391a2dcca4f3768
”ikebuchieito” <toei8428gmali.com>
2020-12-03 07:59:13 +0000

    pw commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name,  presence: true, length: { maximum: 15 }
+  validates :email, presence: true, length: { maximum: 300 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
   has_secure_password
- 
+  
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
+  validates :password, presence: true,
+            format: { with: VALID_PASSWORD_REGEX,
+             message: "は半角8~32文字小文字・数字それぞれ１文字以上含む必要があります"}
 end
