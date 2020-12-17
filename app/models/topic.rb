0264fa0f554b5315450cdc38ac81da3a4ed1dbf8
”ikebuchieito” <toei8428gmali.com>
2020-12-17 07:07:14 +0000

    rails07

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,10 +3,11 @@ class Topic < ApplicationRecord
   validates :description, presence: true
   validates :image, presence: true
 
-  belongs_to :user
+  belongs_to :user, optional: true
 
   mount_uploader :image, ImageUploader
   
   has_many :favorites
   has_many :favorite_users, through: :favorites, source: 'user'
+
 end
