1ff7d619d35afdc1aba1b75dfb3266b2ed1f4beb
”ikebuchieito” <toei8428gmali.com>
2020-12-28 10:38:57 +0000

    s3ok

--- config/initializers/carrierwavve.rb
+++ config/initializers/carrierwavve.rb
@@ -3,15 +3,14 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
+  config.storage :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_directory  = ENV['rails-pictgram']
   config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: 'AKIARRS33554RADE7Y4W',
-      aws_secret_access_key: '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',
-      region: 'ap-northeast-1'
-    }
-
-    config.fog_directory  = 'rails-pictgram'
-    config.cache_storage = :fog
-   
-  
+    provider: 'AWS',
+    aws_access_key_id: ENV['AKIARRS33554RADE7Y4W'],
+    aws_secret_access_key: ENV['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'],
+    region: ENV['ap-northeast-1'],
+    path_style: true
+  }
 end
